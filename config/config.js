e8428e90cbb5baf8bd564297adee924da00d9c8b
SOFIA <rusofiaru@gmail.com>
2020-11-27 22:37:02 +0200

    config with test db

--- config/config.js
+++ config/config.js
@@ -1,7 +1,13 @@
 let config = {};
 
 if (Deno.env.get('TEST_ENVIRONMENT')) {
-    config.database = {};
+    config.database = {
+        hostname: "suleiman.db.elephantsql.com",
+        database: "gtkemmpz",
+        user: "gtkemmpz",
+        password: "EOOLlgeXgpwzhJEZKZFltVNmgcNKQ3rG",
+        port: 5432
+    };
 } else {
     config.database = {
         hostname: "suleiman.db.elephantsql.com",
