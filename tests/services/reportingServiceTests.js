bc9518928c4c3c292a3e078e3c4c9e14723b5f07
Alex Banul <aleksandr.banul@avanade.com>
2020-11-28 20:50:34 +0100

    Reporting service tests for morning info

---
+++ tests/services/reportingServiceTests.js
@@ -0,0 +1,71 @@
+import {assert, assertEquals} from '../deps.js'
+import {deleteMorningReport, getMorningReport, postMorningReport} from '../../services/reportingService.js'
+import {createUser, deleteUser} from "../../services/authService.js";
+
+Deno.test('getMorningReport should return null when no data has been found', async () => {
+    // assert
+    const report = await getMorningReport('2020-01-15', -1)
+
+    // assert
+    assertEquals(report, null)
+})
+
+Deno.test('postMorningInfo should persist morning report correctly', async () => {
+    // arrange
+    const email = 'testMorningReport1@test.com';
+    const userId = await createUser(email, '12345')
+    const day = '2020-01-15';
+
+    try {
+        const morningInfo = {
+            sleepDuration: '8',
+            sleepQuality: '100',
+            mood: '5',
+            day: day,
+            userId: userId
+        }
+
+        // act
+        await postMorningReport(morningInfo)
+        const actual = await getMorningReport(day, userId)
+
+        // assert
+        assertEquals(actual, morningInfo)
+    } finally {
+        // teardown
+        await deleteMorningReport(day, userId)
+        await deleteUser(email)
+    }
+})
+
+Deno.test('deleteMorningInfo should delete morning report correctly', async () => {
+    // arrange
+    const email = 'testMorningReport2@test.com';
+    const userId = await createUser(email, '12345')
+    const day = '2020-01-15';
+
+    try {
+        const morningInfo = {
+            sleepDuration: '8',
+            sleepQuality: '100',
+            mood: '5',
+            day: day,
+            userId: userId
+        }
+
+        await postMorningReport(morningInfo)
+        let report = await getMorningReport(day, userId)
+
+        assert(report != null, 'Can not find any report to delete!')
+
+        // act
+        await deleteMorningReport(day, userId)
+        report = await getMorningReport(day, userId)
+
+        // assert
+        assertEquals(report, null, 'Report was not deleted!')
+    } finally {
+        // teardown
+        await deleteUser(email)
+    }
+})
