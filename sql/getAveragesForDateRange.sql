ab44194087aa8d595be36009b70f786910dc7716
Alex Banul <aleksandr.banul@avanade.com>
2020-11-29 02:13:47 +0100

    add get_averages_for_date_range function

--- sql/getAveragesForDateRange.sql
+++ sql/getAveragesForDateRange.sql
@@ -1,6 +1,6 @@
-DROP FUNCTION IF EXISTS get_averages_for_user;
+DROP FUNCTION IF EXISTS get_averages_for_date_range;
 
-CREATE OR REPLACE FUNCTION get_averages_for_user(_user_id INT, _from_date DATE, _to_date DATE)
+CREATE OR REPLACE FUNCTION get_averages_for_date_range(_from_date DATE, _to_date DATE)
     RETURNS TABLE
             (
                 avg_mood              NUMERIC,
@@ -16,41 +16,31 @@ $func$
 BEGIN
     RETURN QUERY
         WITH avgMood AS (
-            SELECT avg(M.mood) AS avg_mood, M.user_id
-            FROM (SELECT mi.mood, mi.user_id
+            SELECT avg(M.mood) AS avg_mood
+            FROM (SELECT mi.mood
                   FROM morning_info mi
-                  WHERE mi.user_id = _user_id
-                    AND mi.day BETWEEN _from_date AND _to_date
+                  WHERE mi.day BETWEEN _from_date AND _to_date
 
                   UNION
 
-                  SELECT ei.mood, ei.user_id
+                  SELECT ei.mood
                   FROM evening_info ei
-                  WHERE ei.user_id = _user_id
-                    AND ei.day BETWEEN _from_date AND _to_date) M
-            GROUP BY M.user_id
+                  WHERE ei.day BETWEEN _from_date AND _to_date) M
         ),
              avgEI AS (
                  SELECT avg(ei.eating_quality)    AS avg_eating_quality,
                         avg(ei.eating_regularity) AS avg_eating_regularity,
                         avg(ei.exercise_time)     AS avg_exercise_time,
-                        avg(ei.studying_time)     AS avg_studying_time,
-                        ei.user_id
+                        avg(ei.studying_time)     AS avg_studying_time
                  FROM evening_info ei
-                 WHERE ei.user_id = _user_id
-                   AND ei.day BETWEEN _from_date AND _to_date
-                 GROUP BY ei.user_id
+                 WHERE ei.day BETWEEN _from_date AND _to_date
              ),
              avgMI AS (
                  SELECT avg(mi.sleep_quality)  AS avg_sleep_quality,
-                        avg(mi.sleep_duration) AS avg_sleep_duration,
-                        mi.user_id
+                        avg(mi.sleep_duration) AS avg_sleep_duration
                  FROM morning_info mi
-                 WHERE mi.user_id = _user_id
-                   AND mi.day BETWEEN _from_date AND _to_date
-                 GROUP BY mi.user_id)
-
-
+                 WHERE mi.day BETWEEN _from_date AND _to_date
+             )
         SELECT m.avg_mood,
                ei.avg_eating_quality,
                ei.avg_eating_regularity,
@@ -58,10 +48,8 @@ BEGIN
                ei.avg_studying_time,
                mi.avg_sleep_quality,
                mi.avg_sleep_duration
-        FROM avgMood m
-                 FULL JOIN avgEI ei
-                           ON m.user_id = ei.user_id
-                 FULL JOIN avgMI mi
-                           ON m.user_id = mi.user_id;
+        FROM avgMood m,
+             avgEI ei,
+             avgMI mi;
 END
 $func$ LANGUAGE plpgsql;
